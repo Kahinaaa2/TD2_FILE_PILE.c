3cadb9e20bd45ac2bc0b6be7600526933ea10cc7
HAMMAD Kahina <166177981+Kahinaaa2@users.noreply.github.com>
2024-11-01 16:31:13 +0100

    Create Exercice1.c

---
+++ Exercice1.c
@@ -0,0 +1,20 @@
+Exercice 1 (Application du cours)
+1. Soit la pile suivante :
+[2, 5, 9, 10, 1, -3]
+avec -3 le haut de la pile. Indiquez l’état de la pile une fois les opérations suivantes effectuées
+— Dépiler
+— Dépiler
+— Empiler 12
+— Dépiler
+— Empiler 8
+— Empiler 7
+(a) Dans cet ordre
+(b) Dans l’ordre inverse
+2. Mêmes questions si la liste présentée est une file avec l’équivalence empiler/enfiler, dépiler/défiler.
+3. Comment pourrait-on simuler une file à l’aide de piles ?
+
+Correction:
+1-a [2 5 9 10 8 7]
+1-b [2, 5, 9, 10, 1, -3]
+2-a [7 8 12 2 5 9]
+3-avec deux piles
